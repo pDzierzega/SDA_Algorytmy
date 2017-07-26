7d87633d2a2c747aa46720519bf487f84885a2d0
Paweł Dzierżęga <pawel.dzierzega@outlook.com>
2017-07-26 21:01:17 +0200

    Usunalem branche

--- Algorytmy/src/Alghoritms/ZliczaczLiter.java
+++ Algorytmy/src/Alghoritms/ZliczaczLiter.java
@@ -26,6 +26,7 @@ public class ZliczaczLiter extends AbstractAlgorithm {
             countChar(character);
         }
         sortedAndPrint();
+        
     }
 
     private void sortedAndPrint() {
