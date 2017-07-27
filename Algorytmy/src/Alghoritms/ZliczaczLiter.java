09f97a606bf52d37ecc814349b8d9f8a9eb11b4e
Paweł Dzierżęga <pawel.dzierzega@outlook.com>
2017-07-27 17:34:53 +0200

    commit, zmieniem plik gitignore

--- Algorytmy/src/Alghoritms/ZliczaczLiter.java
+++ Algorytmy/src/Alghoritms/ZliczaczLiter.java
@@ -26,7 +26,7 @@ public class ZliczaczLiter extends AbstractAlgorithm {
             countChar(character);
         }
         sortedAndPrint();
-        
+
     }
 
     private void sortedAndPrint() {
